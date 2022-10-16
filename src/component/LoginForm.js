00d29f56dabb3ebdecc499aea218ed257a284150
hthanh12 <nghoangthanh1212@gmail.com>
2022-10-16 15:09:08 +0700

    add version

--- src/component/LoginForm.js
+++ src/component/LoginForm.js
@@ -1,89 +1,110 @@
-import { useState } from 'react'
-import { Avatar, CssBaseline, TextField, Link, Paper, Box, Grid, Typography, InputLabel, MenuItem, FormControl, Select, Stack } from '@mui/material'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import LoadingButton from '@mui/lab/LoadingButton'
-import axios from 'axios'
-import { TEAM_MEMBER, TEAM_NAME, SERVERS } from '../utils/data'
+import { useState } from "react";
+import {
+  Avatar,
+  CssBaseline,
+  TextField,
+  Link,
+  Paper,
+  Box,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+  Stack,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import LoadingButton from "@mui/lab/LoadingButton";
+import axios from "axios";
+import { TEAM_MEMBER, TEAM_NAME, SERVERS, VERSION } from "../utils/data";
 
 const userDetault = {
   email: process.env.REACT_APP_USER_IWS,
   password: process.env.REACT_APP_PWD_IWS,
-}
+};
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://github.com/hthanh12">
         Thành Uchiha - {TEAM_NAME}
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
-  )
+  );
 }
 
 let getRandomBackground = () => {
-  let array = [{
-    id: 1,
-    name: 'team',
-    url: 'https://media.discordapp.net/attachments/752362298831667332/983121266842173530/QR.jpg'
-  },]
+  let array = [
+    {
+      id: 1,
+      name: "team",
+      url: "https://media.discordapp.net/attachments/752362298831667332/983121266842173530/QR.jpg",
+    },
+  ];
 
-
-
-  return array[Math.floor(Math.random() * array.length)].url
-}
-const theme = createTheme()
+  return array[Math.floor(Math.random() * array.length)].url;
+};
+const theme = createTheme();
 
 export default function LoginForm() {
-  const [server, setServer] = useState(SERVERS[0])
-  const [loading, setLoading] = useState(false)
+  const [server, setServer] = useState(SERVERS[0]);
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (event) => {
-    let findSever = SERVERS.find((o) => o.id * 1 === event.target.value * 1)
-    console.log(findSever)
-    setServer(findSever)
-  }
+    let findSever = SERVERS.find((o) => o.id * 1 === event.target.value * 1);
+    console.log(findSever);
+    setServer(findSever);
+  };
 
   const handleSubmit = (event) => {
-    setLoading(true)
-    event.preventDefault()
-    const data = new FormData(event.currentTarget)
+    setLoading(true);
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
     let dataLogin = JSON.stringify({
-      email: data.get('email'),
-      password: data.get('password'),
-    })
-    console.log(dataLogin)
-    const findServer = SERVERS.find((o) => o.id * 1 === data.get('server') * 1)
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+    console.log(dataLogin);
+    const findServer = SERVERS.find((o) => o.id * 1 === data.get("server") * 1);
 
     var config = {
-      method: 'post',
-      url: findServer.link + '/login',
+      method: "post",
+      url: findServer.link + "/login",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      data: dataLogin
-    }
+      data: dataLogin,
+    };
 
     axios(config)
       .then(function (response) {
-        console.log(JSON.stringify(response.data))
-        localStorage.setItem('iws', JSON.stringify(response.data))
-        localStorage.setItem('env', JSON.stringify(findServer.name))
-        setLoading(false)
+        console.log(JSON.stringify(response.data));
+        localStorage.setItem("iws", JSON.stringify(response.data));
+        localStorage.setItem("env", JSON.stringify(findServer.name));
+        setLoading(false);
         window.location.reload(true);
       })
       .catch(function (error) {
-        alert('Nhập sai mật khẩu hoặc tài khoản rồi bạn eiiiiiiiiiiiiiiiii\n' + error.message)
-        localStorage.setItem('iws', null)
-        setLoading(false)
-      })
-
-  }
-  let urlBG = getRandomBackground()
+        alert(
+          "Nhập sai mật khẩu hoặc tài khoản rồi bạn eiiiiiiiiiiiiiiiii\n" +
+            error.message
+        );
+        localStorage.setItem("iws", null);
+        setLoading(false);
+      });
+  };
+  let urlBG = getRandomBackground();
   return (
     <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
         <CssBaseline />
         <Grid
           item
@@ -92,11 +113,13 @@ export default function LoginForm() {
           md={7}
           sx={{
             backgroundImage: `url(${urlBG})`,
-            backgroundRepeat: 'no-repeat',
+            backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
           }}
         />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
@@ -104,21 +127,32 @@ export default function LoginForm() {
             sx={{
               my: 8,
               mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
             {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
               <LockOutlinedIcon />
             </Avatar> */}
             <Stack direction="row" spacing={2}>
-              {TEAM_MEMBER.map((member) => <Avatar key={member.id} alt={member.name} src={member.avt}></Avatar>)}
+              {TEAM_MEMBER.map((member) => (
+                <Avatar
+                  key={member.id}
+                  alt={member.name}
+                  src={member.avt}
+                ></Avatar>
+              ))}
             </Stack>
             <Typography component="h1" variant="h5">
               {TEAM_NAME} - HIP HOP NEVER DIE
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
               <TextField
                 margin="normal"
                 required
@@ -143,18 +177,23 @@ export default function LoginForm() {
               />
               <Box sx={{ minWidth: 120, mt: 1 }}>
                 <FormControl fullWidth>
-                  <InputLabel id="demo-simple-select-label">Select Sever</InputLabel>
+                  <InputLabel id="demo-simple-select-label">
+                    Select Sever
+                  </InputLabel>
                   <Select
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
-                    value={server.id ?? ''}
+                    value={server.id ?? ""}
                     label="server"
                     onChange={handleChange}
                     name="server"
                   >
-                    {
-                      SERVERS.map((server) => <MenuItem key={server.id} value={server.id}> {server.name}</MenuItem>)
-                    }
+                    {SERVERS.map((server) => (
+                      <MenuItem key={server.id} value={server.id}>
+                        {" "}
+                        {server.name}
+                      </MenuItem>
+                    ))}
                   </Select>
                 </FormControl>
               </Box>
@@ -185,11 +224,12 @@ export default function LoginForm() {
                   </Link> */}
                 </Grid>
               </Grid>
+              <h5 sx={{ mt: 5 }}>Version: {VERSION}</h5>
               <Copyright sx={{ mt: 5 }} />
             </Box>
           </Box>
         </Grid>
       </Grid>
     </ThemeProvider>
-  )
+  );
 }
